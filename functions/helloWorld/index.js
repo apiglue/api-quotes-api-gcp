b10ae05badfae26b910d3775854809870ed91f14
Marcelo <marcelo79@gmail.com>
2018-07-26 15:08:53 -0400

    Update index.js

--- functions/helloWorld/index.js
+++ functions/helloWorld/index.js
@@ -4,6 +4,26 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
+
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
+
 exports.helloHttp = (req, res) => {
+db.collection('users').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+
+    
     res.send(`Hello ${req.body.name || 'World'}!`);
+    
   };
